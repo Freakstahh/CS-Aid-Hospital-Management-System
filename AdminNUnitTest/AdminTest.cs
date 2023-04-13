545c7b00635715a10cb23d5af1faa1bb28a86e61
Dan Marco Zabala <21-1-00241@vsu.edu.ph>
2023-04-13 21:06:38 +0800

    fixed adminTest

--- AdminNUnitTest/AdminTest.cs
+++ AdminNUnitTest/AdminTest.cs
@@ -19,7 +19,7 @@ namespace AdminNUnitTest
 
             // Assert
             Assert.AreEqual(initialCount + 1, admin.CountNumberOfPatients());
-            Assert.IsTrue(admin.Patients.Contains(patient));
+            Assert.IsTrue(admin.patients.Contains(patient));
             Assert.AreEqual(1, building.Rooms[0].State);
         }
 
@@ -38,7 +38,7 @@ namespace AdminNUnitTest
 
             // Assert
             Assert.AreEqual(initialCount - 1, admin.CountNumberOfPatients());
-            Assert.IsFalse(admin.Patients.Contains(patient));
+            Assert.IsFalse(admin.patients.Contains(patient));
             Assert.AreEqual(0, building.Rooms[0].State);
         }
 
@@ -48,14 +48,14 @@ namespace AdminNUnitTest
             // Arrange
             Admin admin = new Admin("AD12345", "John Doe", "M", DateTime.Now.AddYears(-30), "1234567890", "johndoe@example.com", "johndoe", "password", "Day", "Cardiology", "12345");
             Doctor doctor = new Doctor("DOC12345", "Jane Doe", "F", DateTime.Now.AddYears(-35), "1234567890", "janedoe@example.com", "janedoe", "password", "Day", "Cardiology", "Cardiologist", "01");
-            int initialCount = admin.Doctors.Count;
+            int initialCount = admin.doctors.Count;
 
             // Act
             admin.addDoctor(doctor);
 
             // Assert
-            Assert.AreEqual(initialCount + 1, admin.Doctors.Count);
-            Assert.IsTrue(admin.Doctors.Contains(doctor));
+            Assert.AreEqual(initialCount + 1, admin.doctors.Count);
+            Assert.IsTrue(admin.doctors.Contains(doctor));
         }
 
         [Test]
@@ -64,14 +64,14 @@ namespace AdminNUnitTest
             // Arrange
             Admin admin = new Admin("AD12345", "John Doe", "M", DateTime.Now.AddYears(-30), "1234567890", "johndoe@example.com", "johndoe", "password", "Day", "Cardiology", "12345");
             Nurse nurse = new Nurse("NS12345", "Mary Smith", "F", DateTime.Now.AddYears(-25), "1234567890", "marysmith@example.com", "marysmith", "password", "Day", "Cardiology", "02");
-            int initialCount = admin.Nurses.Count;
+            int initialCount = admin.nurses.Count;
 
             // Act
             admin.addNurse(nurse);
 
             // Assert
-            Assert.AreEqual(initialCount + 1, admin.Nurses.Count);
-            Assert.IsTrue(admin.Nurses.Contains(nurse));
+            Assert.AreEqual(initialCount + 1, admin.nurses.Count);
+            Assert.IsTrue(admin.nurses.Contains(nurse));
         }
     }
 }
--- CS AID HOSPITAL MANAGEMENT SYTEM/Admin.cs
+++ CS AID HOSPITAL MANAGEMENT SYTEM/Admin.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CS_Aid_Hospital_Management_System
 {
-    public class Admin: User
+    public class Admin : User
     {
+        [JsonRequired]
         internal List<Patient> Patients = new List<Patient>();
+        [JsonIgnore]
+        public List<Patient> patients = new List<Patient>();
+
+        [JsonRequired]
         internal List<Doctor> Doctors = new List<Doctor>();
-        internal List <Nurse> Nurses = new List<Nurse>();
+        [JsonIgnore]
+        public List<Doctor> doctors = new List<Doctor>();
+
+        [JsonRequired]
+        internal List<Nurse> Nurses = new List<Nurse>();
+        [JsonIgnore]
+        public List<Nurse> nurses = new List<Nurse>();
 
         public Admin(string id, string name, string gender, DateTime birthdate, string contactNumber, string email, string username, string password, string shifts, string departments, string uniqueKey) : base(id,name, gender, birthdate, contactNumber, email, username, password, shifts, departments, uniqueKey)
         {
